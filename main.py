647a7db3c0732462be6f34e390e37a5640ac452c
Vlad <vlad.burko100@gmail.com>
2026-06-08 17:49:59 +0300

    created log in, log out and cookie

--- main.py
+++ main.py
@@ -1,49 +1,122 @@
 import asyncio
-
-from fastapi import FastAPI, Request, Form
+from fastapi import Depends, FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from psutil import users
 from utils.templates import templates
 import uvicorn
+from sqlalchemy.orm import Session
 from bot import send_application
-from model import Users
+from pydantic_schema import Users
+from database import engine, get_db, Base
+from models import User
+from auth import get_password_hash, verify_password
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get('/', name='home')
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    username = request.cookies.get("username")
+    return templates.TemplateResponse("index.html", {"request": request, "username": username})
 
-@app.get('/form', name='form')
-def form(request: Request):
-    return templates.TemplateResponse(name="forms.html", request=request)
 
-@app.post('/form', name='submit_form')
-async def submit_form(request: Request,
-    phone: str = Form(...),
-    name: str = Form(...),
-    fullname: str = Form(...)):
-    users = Users(name=name, fullname=fullname, phone=phone)
-    
+@app.get('/application', name='application')
+def application(request: Request):
+    username = request.cookies.get("username")
+    return templates.TemplateResponse("application.html", {
+        "request": request,
+        "username": username
+    })
+
+
+@app.post('/application', name='submit_form')
+async def submit_form(request: Request, phone: str = Form(...), name: str = Form(...), fullname: str = Form(...)):
+    username = request.cookies.get("username")    
     if len(phone) < 18:
-        return templates.TemplateResponse("forms.html", {
+        return templates.TemplateResponse("application.html", {
             "request": request,
             "error": "Неверный формат номера телефона",
             "phone": phone,
             "name": name,
-            "fullname": fullname
+            "fullname": fullname,
+            "username": username,
+        })
+    else:
+        asyncio.create_task(send_application(phone, name, fullname))
+
+        return templates.TemplateResponse("success.html", {
+            "request": request, 
+            "phone": phone, 
+            "name": name, 
+            "fullname": fullname,
+            "username": username,
         })
 
-    asyncio.create_task(send_application(users.phone, users.name, users.fullname))
 
-    return templates.TemplateResponse("success.html", {
-        "request": request, 
-        "phone": users.phone, 
-        "name": users.name, 
-        "fullname": users.fullname
-    })
+@app.get('/register', name='register')
+def register(request: Request):
+    return templates.TemplateResponse(name="register.html", request=request)
+
+
+@app.post('/register', name='register_user')
+def register_user(request: Request, email: str = Form(...), username: str = Form(...), password: str = Form(...), confirm_password: str = Form(...), db: Session = Depends(get_db)):
+    if password != confirm_password:
+        return templates.TemplateResponse("register.html", {
+            "request": request,
+            "error": "Пароли не совпадают",
+            "username": username
+        })
+
+    new_user = db.query(User).filter(User.email == email).first()
+    if new_user:
+        return templates.TemplateResponse("register.html", {
+            "request": request,
+            "error": "Пользователь уже существует",
+        })
+    
+    new_user = User(
+        email=email,
+        username=username,
+        password=get_password_hash(password)
+    )
+    db.add(new_user)
+    db.commit()
+
+    return RedirectResponse(url="/login", status_code=303)
+
+
+@app.get('/login')
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.post('/login', name='login_user')
+def login_user(request: Request, email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user or not verify_password(password, user.password):
+        return templates.TemplateResponse("login.html", {
+            "request": request,
+            "error": "Неверный email или пароль",
+            "email": email
+        })
+    
+    response = RedirectResponse(url='/', status_code=303)
+    response.set_cookie(key="username", value=user.username)
+    return response
+
+
+@app.get("/logout")
+def logout():
+    response = RedirectResponse(url='/', status_code=303)
+    response.delete_cookie("username")
+    return response
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
